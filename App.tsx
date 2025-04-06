7597cea957e8c2e47e8bc25a5fa30e6096203f60
suzzang2 <subin030102@naver.com>
2025-04-06 19:22:41 +0900

    FEAT: App.tsx 정리 & navigation으로 감싸줌

--- App.tsx
+++ App.tsx
@@ -1,46 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import MainPage from './src/pages/MainPage';
+import {NavigationContainer} from '@react-navigation/native';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: '#222831',
-          }}>
-          <MainPage />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <MainPage />
+    </NavigationContainer>
   );
 }
 
