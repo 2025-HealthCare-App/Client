e4b787648fd56fc09a0cf2f16a277cc5fd51b2c4
suzzang2 <subin030102@naver.com>
2025-04-07 20:10:11 +0900

    FEAT: navigation 테스트 완료

--- App.tsx
+++ App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-
-import MainPage from './src/pages/MainPage';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import MainScreen from './src/screens/MainScreen';
+import LoginScreen from './src/screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <MainPage />
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Main"
+          component={MainScreen}
+          // options={{title: 'Welcome'}}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          // options={{title: 'Welcome'}}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
