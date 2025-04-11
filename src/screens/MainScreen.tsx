b81314fd8405127e46aabc2d536206fee3bf045e
suzzang2 <subin030102@naver.com>
2025-04-11 21:55:46 +0900

    FEAT: TextTicker 완성

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {Image} from 'react-native'; // ✅ 이거 꼭 필요!
 import styled from 'styled-components/native';
 import UserBar from '../components/mainScreen/UserBar';
+import TextTicker from '../components/mainScreen/TextTicker';
 
 const MainScreen = ({navigation}: {navigation: any}) => {
   return (
     <Wrapper>
       <UserBar />
+      <TextTicker />
     </Wrapper>
   );
 };
@@ -18,5 +20,6 @@ const Wrapper = styled.View`
   justify-content: flex-start;
   align-items: center;
   background-color: #222831;
-  padding: 20px 20px;
+  padding: 20px 0;
+  gap: 10px;
 `;
