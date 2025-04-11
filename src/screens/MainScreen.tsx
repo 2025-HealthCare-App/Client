2e0e67794f98306667fb6bbd183085875adae9aa
suzzang2 <subin030102@naver.com>
2025-04-11 21:36:43 +0900

    FEAT: UserBar 완성

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import {Image} from 'react-native'; // ✅ 이거 꼭 필요!
 import styled from 'styled-components/native';
+import UserBar from '../components/mainScreen/UserBar';
 
 const MainScreen = ({navigation}: {navigation: any}) => {
   return (
     <Wrapper>
-      {/* <MainButton onPress={() => navigation.navigate('Login')}>
-        <ButtonText>Go to Login</ButtonText>
-      </MainButton> */}
-      <UserBar>
-        <UserInfoContainer>
-          <ProfileImg source={require('../../images/profileImg.jpg')} />
-          <TierBadge source={require('../../images/tierBadge.png')} />
-          <UserName>나는야초보</UserName>
-        </UserInfoContainer>
-        <PointContainer />
-      </UserBar>
+      <UserBar />
     </Wrapper>
   );
 };
@@ -25,52 +16,7 @@ export default MainScreen;
 const Wrapper = styled.View`
   flex: 1;
   justify-content: flex-start;
-  padding-top: 20px;
   align-items: center;
   background-color: #222831;
-`;
-
-const UserBar = styled.View`
-  width: 100%;
-  height: 70px;
-  /* border-bottom-width: 1px;
-  border-bottom-color: #ffffff; */
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 20px;
-`;
-
-const UserInfoContainer = styled.View`
-  width: 50%;
-  height: 100%;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  background-color: #00494d;
-  border: 3px solid #02adb5;
-  border-radius: 50px;
-  padding: 5px 10px;
-`;
-
-const PointContainer = styled.View`
-  flex-direction: row;
-  align-items: center;
-`;
-
-const ProfileImg = styled(Image)`
-  width: 50px;
-  height: 50px;
-  border-radius: 50px;
-  /* border: 1.5px solid #fff; */
-`;
-const TierBadge = styled(Image)`
-  width: 30px;
-  height: 30px;
-`;
-const UserName = styled.Text`
-  font-size: 15px;
-  color: #ffffff;
-  margin-left: 10px;
-  font-weight: bold;
+  padding: 20px 20px;
 `;
---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+// src/styles/theme.ts
+import {DefaultTheme} from 'styled-components/native'; // ✅ native!!
+
+export const theme: DefaultTheme = {
+  colors: {
+    primary: '#02adb5',
+    background: '#222831',
+    text: '#ffffff',
+  },
+  fonts: {
+    regular: 'Pretendard',
+    bold: 'Pretendard',
+  },
+};
