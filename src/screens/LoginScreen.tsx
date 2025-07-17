19f249293659b2f365a1fc27f7ebdc709fdb304c
suzzang2 <subin030102@naver.com>
2025-07-17 13:06:21 +0900

    ADD: jsonwebtoken 의존성 설치

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -37,6 +37,10 @@ const LoginScreen = () => {
               .catch(error => {
                 console.error('로그인 실패:', error);
                 // 로그인 실패 처리
+                Alert.alert(
+                  '로그인 실패',
+                  '아이디 또는 비밀번호가 잘못되었습니다.',
+                );
               });
           }}>
           <LoginText>로그인</LoginText>
