ac3cded8f5b6b2265859c71571f7ff9f20f5cffa
suzzang2 <subin030102@naver.com>
2025-09-21 17:00:10 +0900

    ADD: ~StackNav.tsx 파일들 생성

---
+++ src/navigators/MainStackNav.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import MainScreen from '../screens/MainScreen';
+
+const Stack = createNativeStackNavigator();
+
+
+const MainStackNavigator = () => {
