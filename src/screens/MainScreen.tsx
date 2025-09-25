8445ee3654d5149cfb6dc60e30f4e0b70855627d
suzzang2 <subin030102@naver.com>
2025-09-26 01:10:59 +0900

    DELETE: 임시 버튼 삭제

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -4,19 +4,12 @@ import UserBar from '../components/mainScreen/UserBar';
 import StyledTextTicker from '../components/mainScreen/StyledTextTicker';
 import MainContents from '../components/mainScreen/MainContents';
 import useWeeklyRewardAlert from '../hooks/useWeeklyRewardAlert';
-import {Button} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
 
 const MainScreen = () => {
   useWeeklyRewardAlert();
-  const navigation = useNavigation<any>();
 
   return (
     <Wrapper>
-      <Button
-        title="Pedometer 테스트 화면으로"
-        onPress={() => navigation.navigate('PedometerTest')}
-      />
       <UserBar />
       <StyledTextTicker text="이번주 목표를 달성할 수 있을까요? 오늘도 함께 달려요!  이번주 목표를 달성할 수 있을까요? 오늘도 함께 달려요!" />
       <MainContents />
